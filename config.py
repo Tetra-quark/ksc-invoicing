a320e56d4e88b94a2c521af5115f27d0514d51c4
jkopp <jeffreykoppanyi@gmail.com>
2023-05-09 16:36:27 +0200

    add generate invoice main method with config

---
+++ config.py
@@ -0,0 +1,46 @@
+personal_details = {
+    "first_name":       "Jeffrey",
+    "family_name":      "Koppanyi",
+    "fullname":         "Jeffrey Koppanyi",
+    "email":            "jeffreykoppanyi@gmail.com",
+    "address":      "454 Route de Véségnin; 01280 Prévessin-Moëns, France",
+    "phone":            "(+33) 0766302324",
+    "website": "somewebsite.com"
+}
+
+company_details = {
+    "name":     "Koppanyi Scientific Consulting",
+    "SIRET":    "90979545200012",
+    "SIREN":    "909795452",
+}
+
+client_info = {
+    "GDA": {
+        "fullname": "Global Dressage Analytics",
+        "email": "john@globaldressageanalytics.com",
+        "address": "34 Schijndelseweg; 5192TB Sint-Oedenrode, Pays-Bas",
+    },
+    "BelleFerme": {
+        "fullname": "Sports Equestres de Belle Ferme",
+        "email": "carolyn@belleferme.com",
+        "address": "555 Avenue de Belle Ferme; 01170 Gex, France",
+    }
+}
+
+invoice_items = [
+    # {
+    #     "description": "Prestation comptabilité et programmation (heure)",
+    #     "quantity": 60,
+    #     "price_per_unit": 15,
+    # },
+    # {
+    #     "description": "Prestation administrative et comptabilité informatique (heure)",
+    #     "quantity": 33,
+    #     "price_per_unit": 15,
+    # },
+    {
+        "description": "Prestation R+D, programmation & analyse des donnés (heure)",
+        "quantity": 99,
+        "price_per_unit": 15
+    },
+]
