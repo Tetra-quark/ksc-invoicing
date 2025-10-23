dd1f2dcddd1f7cca271d189ed4809af7d121cba8
jkopp <jeffreykoppanyi@gmail.com>
2025-10-23 15:12:58 +0200

    Update invoice building to use functions. Refactor out json data extraction functions for the invoice builder.

--- generate_invoice_from_json.py
+++ generate_invoice_from_json.py
@@ -12,7 +12,7 @@ import argparse
 from kscinvoicing.info.address import Address
 from kscinvoicing.info.contactinfo import ContactInfo
 from kscinvoicing.invoice import LineItem, Invoice, InvoiceLogger
-from kscinvoicing.pdf.invoicebuilder import InvoiceBuilder
+from kscinvoicing.pdf.invoicebuilder import save_document, build_invoice
 
 
 def invoice_data_from_json(path: str) -> dict:
@@ -36,6 +36,34 @@ def preview_file(draftpath: Path):
         print(f"Preview unavailable for OS: {system}. Please open the file manually: {draftpath}")
 
 
+def extract_sender_from_json(data: dict) -> ContactInfo:
+    sender = ContactInfo(name=data['sender']['name'],
+                         address=Address(**data['sender']['address']),
+                         email=data['sender']['email'],
+                         phone=data['sender']['phone'],
+                         website=data['sender']['website'])
+    return sender
+
+
+def extract_recipient_from_json(data: dict) -> ContactInfo:
+    recipient = ContactInfo(name=data['recipient']['name'],
+                            address=Address(**data['recipient']['address']),
+                            email=data['recipient']['email'],
+                            phone=data['recipient']['phone'],
+                            )
+    return recipient
+
+
+def extract_lineitems_from_json(data: dict) -> list[LineItem]:
+    items = []
+    for item in data['lineitem_details']:
+        items.append(LineItem(description=item['description'],
+                              quantity=item['quantity'],
+                              price_per_unit=Decimal(item['price_per_unit'])
+                              ))
+    return items
+
+
 def main():
 
     parser = argparse.ArgumentParser(description="Generate invoice from json file.")
@@ -43,52 +71,38 @@ def main():
     parser.add_argument("--no-preview", action='store_false', help="open generated draft invoice in default pdf viewer")
     args = parser.parse_args()
 
-    data = invoice_data_from_json(args.filepath)
     show_preview = args.no_preview
+    data = invoice_data_from_json(args.filepath)
 
-    invoice_date = datetime.strptime(data['invoice_date'], '%Y-%m-%d')
-
-    # create sender
-    sender = ContactInfo(name=data['sender']['name'],
-                         address=Address(**data['sender']['address']),
-                         email=data['sender']['email'],
-                         phone=data['sender']['phone'],
-                         website=data['sender']['website'])
-
-    # create recipient
-    recipient = ContactInfo(name=data['recipient']['name'],
-                            address=Address(**data['recipient']['address']),
-                            email=data['recipient']['email'],
-                            phone=data['recipient']['phone'],
-                            )
-
-    items = []
-    for item in data['lineitem_details']:
-        items.append(LineItem(description=item['description'],
-                              quantity=item['quantity'],
-                              price_per_unit=Decimal(item['price_per_unit'])
-                              ))
 
     invoices_path = Path(data['save_location'])
-    logpath = f"{invoices_path}/log.json"
-    logger = InvoiceLogger(logpath)
+    logger = InvoiceLogger(invoices_path / "log.json")
+
+    invoice_date = datetime.strptime(data['invoice_date'], '%Y-%m-%d')
+    sender = extract_sender_from_json(data)
+    recipient = extract_recipient_from_json(data)
+    items = extract_lineitems_from_json(data)
 
     # create invoice
     invoice = Invoice(items=items,
                       date=invoice_date,
                       invoice_number=logger.invoice_number)
 
-    builder = InvoiceBuilder()
-    pdf_invoice = builder.build_invoice(siren_number=data['sender']['siren'],
-                                        company_name=data['sender']['company'],
-                                        sender=sender,
-                                        recipient=recipient,
-                                        invoice=invoice,
-                                        logopath=data['logo_path'],
-                                        footer_text=data['footer_text'],
-                                        )
 
-    invoice_name = generate_invoice_name(logger.invoice_number, recipient.name, invoice.date)
+    pdf_invoice = build_invoice(
+        siren_number=data['sender']['siren'],
+        company_name=data['sender']['company'],
+        sender=sender,
+        recipient=recipient,
+        invoice=invoice,
+        logopath=data['logo_path'],
+        footer_text=data['footer_text'],
+    )
+
+    invoice_name = invoice.generate_invoice_name(recipient.name)
+
+    save_path = invoices_path / f"{invoice_name}.pdf"
+    save_document(save_path, pdf_invoice)
 
     if show_preview:
 
